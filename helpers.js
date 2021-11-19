a8382043267b7b32fb85424b0f9bb1d32421a4eb
Husni and nijin <93671844+nijin-husni@users.noreply.github.com>
2021-11-19 12:08:44 +0530

    ❤️ᴍᷝᴀⷮᴀᷤɩᴜᴛⷨᴛⷷy💙

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Julie Mwol*:  ```" + msg + "```"
+    return "✅ *Maalutty*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Julie Mwol*:  ```" + msg + "```"
+    return "🛑 *Maalutty*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Julie Mwol*:  ```" + msg + "```"
+    return "⏺️ *Maalutty*:  ```" + msg + "```"
 }
 
 
